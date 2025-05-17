b374774bbeac89dd1ebe7561c38f4add30096f7d
black-cat-32 <blackcatlushee@gmail.com>
2025-05-17 21:35:59 +0000

    Simplified CORS configuration to allow all origins

--- server.js
+++ server.js
@@ -10,18 +10,10 @@ const path = require('path');
 const app = express();
 
 // CORS configuration
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://black-cat-32.github.io');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    
-    // Handle preflight
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
-    next();
-});
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 
 // Middleware
 app.use(express.json());
